4dc507f30e915dd1871a78bec378f6de92931b3d
twoj91 <twoj91@gmail.com>
2013-02-26 16:00:40 +0100

    a

--- skrypt.m
+++ skrypt.m
@@ -1,14 +1,15 @@
 %% wartosci nominalne
 
-Tzewn_n = -20;
-Twew_n = 20;
-Tgz_n = 90;
-Tgp_n = 70;
-Q_n = 20000;
+Tzewn_n = -20;     % [C] temperatura zewnetrzna
+Twew_n = 20;       % [C] temperatura wewnetrzna
+Tgz_n = 90;        % [C] temperatura zasilania grzejnika
+Tgp_n = 70;        % [C] temperatura powrotu z grzejnika
+Q_n = 20000;       % [W] cieplo wymagane
 
 %% parametry pomieszczenia i grzejnika
 
-
+Vg = 0.1;          % [m3] objetosc grzejnika
+Vp = 75;           % [m3] objetosc pomieszczenia
 
 %% parametry fizyczne
 
