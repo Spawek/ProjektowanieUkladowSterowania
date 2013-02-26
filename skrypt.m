4babbbf825d3d2e173dedd4763aa56e4febd450a
twoj91 <twoj91@gmail.com>
2013-02-26 16:47:19 +0100

    parametry

--- skrypt.m
+++ skrypt.m
@@ -1,14 +1,3 @@
-%% wartosci nominalne
-
-Tzewn_n = -20;     % [C] temperatura zewnetrzna
-Twew_n = 20;       % [C] temperatura wewnetrzna
-Tgz_n = 90;        % [C] temperatura zasilania grzejnika
-Tgp_n = 70;        % [C] temperatura powrotu z grzejnika
-Q_n = 20000;       % [W] cieplo wymagane
-Qt_n = 0;          % [W] straty/zyski technologiczne
-
-Tgsr_n = Tgp_n;    % [C] temperatura srednia grzejnika
-
 %% parametry pomieszczenia i grzejnika
 
 Vg = 0.1;          % [m3] objetosc grzejnika
@@ -24,20 +13,34 @@ rop = 1.185;       % [kg / m3] gestosc powietrza
 Cvg = cpw*row*Vg;  % [W] pojemnosc cieplna grzejnika
 Cvw = cpp*rop*Vp;  % [W] pojemnosc cieplna pomieszczenia
 
+%% wartosci nominalne
+
+Tzew_n = -20;      % [C] temperatura zewnetrzna
+Twew_n = 20;       % [C] temperatura wewnetrzna
+Tgz_n = 90;        % [C] temperatura zasilania grzejnika
+Tgp_n = 70;        % [C] temperatura powrotu z grzejnika
+Q_n = 20000;       % [W] cieplo wymagane
+Qt_n = 0;          % [W] straty/zyski technologiczne
+
+Tgsr_n = Tgp_n;                     % [C] temperatura srednia grzejnika
+Fg_n = Q_n/(cpw*row*(Tgz_n-Tgp_n)); % [m3 / s] przeplyw wody przez grzejnik
+
 %% wartosci poczatkowe
 
-Tzewn_0 = Tzewn_n; % [C] temperatura zewnetrzna
+Tzew_0 = Tzew_n;   % [C] temperatura zewnetrzna
 Twew_0 = Twew_n;   % [C] temperatura wewnetrzna
 Tgz_0 = Tgz_n;     % [C] temperatura zasilania grzejnika
 Tgp_0 = Tgp_n;     % [C] temperatura powrotu z grzejnika
+Fg_0 = Fg_n;       % [m3 / s] przeplyw wody przez grzejnik
 
 %% zmiany wartosci
 
 dTzewn = 0;        % [C] zmiana temperatury zewnetrznej
 dTgz = 0;          % [C] zmiana temperatury zasilania grzejnika
 dTgp = 0;          % [C] zmiana temperatury powrotu z grzejnika
+dFg = 0;           % [m3 / s] przeplyw wody przez grzejnik
 
 %% wspolczynniki
 
-kg = Q_n/(Tgsr_n-Twew_n);
-kw = (Q_n-Qt_n)/(Twew_n-Tzew_n);
+kg = Q_n/(Tgsr_n-Twew_n);         % ???
+kw = (Q_n-Qt_n)/(Twew_n-Tzew_n);  % ???
