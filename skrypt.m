78ac3edc544f5ea1e57b3aabb49126af543ab5e9
twoj91 <twoj91@gmail.com>
2013-02-26 15:54:01 +0100

    skrypt

---
+++ skrypt.m
@@ -0,0 +1,19 @@
+%% wartosci nominalne
+
+Tzewn_n = -20;
+Twew_n = 20;
+Tgz_n = 90;
+Tgp_n = 70;
+Q_n = 20000;
+
+%% parametry pomieszczenia i grzejnika
+
+
+
+%% parametry fizyczne
+
+
+
+
+
+%% wartosci poczatkowe
