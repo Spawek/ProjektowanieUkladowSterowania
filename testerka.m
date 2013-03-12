1b8dc43ebcedddeb8c9bbca6a25c4cdd68fc16b9
Spawek <spawnerer@gmail.com>
2013-03-12 14:31:45 +0100

    added model dynamics testing

--- testerka.m
+++ testerka.m
@@ -21,20 +21,39 @@ Q_n = 20000;       % [W] cieplo wymagane
 
 %% zmiany wartosci
 
-dTzew = 0;         % [C] zmiana temperatury zewnetrznej
+dTzew = -20;         % [C] zmiana temperatury zewnetrznej
 dTgz = 0;          % [C] zmiana temperatury zasilania grzejnika
 dFg = 0;           % [m3 / s] przeplyw wody przez grzejnik
 dQt = 0;           % [W] straty/zyski technologiczne
 
 %% symulacje 
-out = symulacja_dyn(Vg, Vp, cpw, row, cpp, rop, Tzew_n, Twew_n, Tgz_n, Tgp_n, Q_n, dTzew-20, dTgz, dFg, dQt);
-
+clf
+Vg = 0.1; 
+out = symulacja_dyn(Vg, Vp, cpw, row, cpp, rop, Tzew_n, Twew_n, Tgz_n, Tgp_n, Q_n, dTzew, dTgz, dFg, dQt);
 subplot(2,1,1)
-plot(out(:,1), out(:,2))
+plot(out(:,1), out(:,2),'r')
 title('temp wewnatrz w czasie')
-
+hold on
 subplot(2,1,2)
-plot(out(:,1), out(:,3))
+plot(out(:,1), out(:,3),'r')
 title('temp grzejnika w czasie')
+hold on
 
+Vg = 0.3; 
+out = symulacja_dyn(Vg, Vp, cpw, row, cpp, rop, Tzew_n, Twew_n, Tgz_n, Tgp_n, Q_n, dTzew, dTgz, dFg, dQt);
+subplot(2,1,1)
+plot(out(:,1), out(:,2),'g')
+subplot(2,1,2)
+plot(out(:,1), out(:,3),'g')
+
+Vg = 0.5; 
+out = symulacja_dyn(Vg, Vp, cpw, row, cpp, rop, Tzew_n, Twew_n, Tgz_n, Tgp_n, Q_n, dTzew, dTgz, dFg, dQt);
+subplot(2,1,1)
+plot(out(:,1), out(:,2),'b')
+subplot(2,1,2)
+plot(out(:,1), out(:,3),'b')
+
+legend('Vgrz = 0.1', 'Vgrz = 0.3', 'Vgrz = 0.5','Location','SouthWest')
+
+hold off
 
